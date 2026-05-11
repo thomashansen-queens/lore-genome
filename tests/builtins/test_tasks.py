ce6fb7746c1289437e15da6d8d79512fe94fc8c4
Aleyssu <alexsunn30@gmail.com>
2026-05-11 16:26:01 -0400

    Some changes to unit tests

--- tests/builtins/test_tasks.py
+++ tests/builtins/test_tasks.py
@@ -1,4 +1,8 @@
 from lore.builtins.tasks.ncbi.fetch_genome_reports import fetch_genome_reports_handler
+from lore.builtins.tasks.data_utils.sampling import sample_handler
 
 def test_fetch_genome_reports(fake_ctx):    
     fetch_genome_reports_handler(fake_ctx, taxons=["Vibrio Cholerae"], search_terms=None, fetch_limit=1)
+
+# def test_sampling(fake_ctx):
+#     sample_handler(fake_ctx)
--- tests/core/test_cache.py
+++ tests/core/test_cache.py
@@ -1,5 +1,6 @@
 from lore.core.cache import TieredCache
 from logging import Logger
 
-def test_l1_cache_creation_deletion():
-    cache = TieredCache(Logger())
+# TODO
+def test_l1_cache_creation_deletion(tmp_path):
+    cache = TieredCache(Logger("Test Logger"), tmp_path)
